c399244bcd20c163bac04d80f936590326c36035
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-03-04 22:05:47 -0300

    Declaracao da classe Funcionario

--- OrientacaoObjeto/src/br/com/orientacaoObjeto/model/Funcionario.java
+++ OrientacaoObjeto/src/br/com/orientacaoObjeto/model/Funcionario.java
@@ -1,5 +1,35 @@
 package br.com.orientacaoObjeto.model;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+	
+	int matricula;
+	String setor;
+	
+	public Funcionario() {
+		super();
+	}
+
+	public Funcionario(int matricula, String setor) {
+		super();
+		this.matricula = matricula;
+		this.setor = setor;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
 
 }
